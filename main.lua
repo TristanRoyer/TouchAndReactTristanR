09b5987fdfdf5c01c0abf479ee566ac448c6acb8
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-03-09 10:45:56 -0500

    not done

--- main.lua
+++ main.lua
@@ -11,15 +11,15 @@ display.setDefault ("background", 153/255, 204/255, 255/255)
 display.setStatusBar(display.HiddenStatusBar)
 
 -- create blue button, set its position and make it visible
-local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png",198 96)
+local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png",198 , 96)
 blueButton.x = display.contentWidth/2
-blueButton.y = display.contenHeight/2
+blueButton.y = display.contentHeight/2
 blueButton.isVisible = true 
 
 -- create red button, set its position and make it invisible
-local redButton = display.newImageRect("Images/Fast Button Active@2x.png"198,96)
+local redButton = display.newImageRect("Images/Fast Button Active@2x.png",198,96)
 redButton.x = display.contentWidth/2
-redbutton.y = display.contentHeight/2
+redButton.y = display.contentHeight/2
 redButton.isVisible = false
 
 -- create text object, set its position and make it invisible
@@ -28,6 +28,18 @@ textObject.x = display.contentWidth/2
 textObject.y = display.contentHeight/3
 textObject:setTextColor (1, 1, 0)
 textObject.isVisible = false
+
+--create the Check mark, set its position and make it invisible
+local checkMark = display.newImageRect("Images/checkmark.png",150,200)
+checkMark.x = display.contentWidth/3
+checkMark.y = display.contentHeight/2
+checkMark.isVisible = false
+
+-- Load two audio streams and one sound
+local laserSound = audio.loadSound( "Correct Answer Sound Effect.mp3" )
+
+local laserChannel = audio.play( laserSound )
+
 
 -- Function: BlueButtonListener
 -- Input: touch listener
@@ -39,20 +51,62 @@ local function BlueButtonListener(touch)
     	blueButton.isVisible = false
     	redButton.isVisible = true
     	textObject.isVisible = true
+    	checkMark.isVisible = true
+     
     end
 
     if (touch.phase == "ended") then
         blueButton.isVisible = true
         redButton.isVisible = false
         textObject.isVisible = false
+        checkMark.isVisible = false
     end
 end
 
 
 
+-- Function: BlueButtonListener
+-- Input: touch listener
+-- Output: none
+-- Description: when blue button is clicked, it will make the text appear 
+-- with the red button, and make the blue button disappear
+local function RedButtonListener(touch)
+    if (touch.phase == "began") then
+    	blueButton.isVisible = true
+    	checkMark.isVisible = false
+    	redButton.isVisible = false
+    	textObject.isVisible = false
+    end
 
+    if (touch.phase == "ended") then
+        blueButton.isVisible = false
+        redButton.isVisible = true
+        textObject.isVisible = true
+        checkMark.isVisible = true
+        correctAnswer = true
+        
+    end
+     
+end
 
+-- Completion listener function
+local function narrationFinished( event )
+    print( "Narration Finished on channel", event.channel )
+    if ( event.completed ) then
+        print( "Narration completed playback naturally" )
+    else
+        print( "Narration was stopped before completion" )
+    end
+end
+ 
 
+ 
 
 
 -- add the respective listeners to each object
+blueButton:addEventListener("touch",BlueButtonListener)
+
+redButton:addEventListener("touch",RedButtonListener)
+
+narrationFinished:addEventListener("eventFrame",narrationFinished)
+
